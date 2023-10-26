4066debcc2c5345117fd410e96ae94beb7dcd72f
SiwaSathya <siwasathya@gmail.com>
2023-10-26 13:01:08 +0800

    update template

--- resources/views/admin-user/index.blade.php
+++ resources/views/admin-user/index.blade.php
@@ -1,185 +1,161 @@
 @extends("template.template")
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Advanced form elements</title>
  @push('css')
   <!-- Bootstrap Color Picker -->
   <link rel="stylesheet" href={{asset("asset/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css")}}>
-  <!-- Select2 -->
-  <link rel="stylesheet" href={{asset("asset/plugins/select2/css/select2.min.css")}}>
-  <link rel="stylesheet" href={{asset("asset/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css")}}>
-  <!-- Bootstrap4 Duallistbox -->
-  <link rel="stylesheet" href={{asset("asset/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css")}}>
-  <!-- BS Stepper -->
-  <link rel="stylesheet" href={{asset("asset/plugins/bs-stepper/css/bs-stepper.min.css")}}>
-  <!-- dropzonejs -->
-  <link rel="stylesheet" href={{asset("asset/plugins/dropzone/min/dropzone.min.css")}}>
-  <!-- Theme style -->
-  <link rel="stylesheet" href={{asset("asset/dist/css/adminlte.min.css")}}>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <link rel="stylesheet" href={{asset("asset/plugins/fontawesome-free/css/all.min.css")}}>
+
+    <link rel="stylesheet" href={{asset("asset/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css")}}>
+    <link rel="stylesheet" href={{asset("asset/plugins/datatables-responsive/css/responsive.bootstrap4.min.css")}}>
+    <link rel="stylesheet" href={{asset("asset/plugins/datatables-buttons/css/buttons.bootstrap4.min.css")}}>
+
+    <link rel="stylesheet" href={{asset("asset/dist/css/adminlte.min.css?v=3.2.0")}}>
+
   @endpush
-</head>
 @section("content")
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+    <div class="content-wrapper">
+    
+    <section class="content-header">
+    <div class="container-fluid">
+    <div class="row mb-2">
+    <div class="col-sm-6">
+    <h1>DataTables</h1>
+    </div>
+    <div class="col-sm-6">
+    <ol class="breadcrumb float-sm-right">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active">DataTables</li>
+    </ol>
+    </div>
+    </div>
+    </div>
+    </section>
+    
+    <section class="content">
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-12">
+    <div class="card">
+    <div class="card-header">
+    
+    
+    <div class="card">
+    <div class="card-header">
+    <h3 class="card-title">DataTable with default features</h3>
+    </div>
+    
+    <div class="card-body">
+    <table id="example1" class="table table-bordered table-striped">
+    <thead>
+    <tr>
+    <th>Rendering engine</th>
+    <th>Browser</th>
+    <th>Platform(s)</th>
+    <th>Engine version</th>
+    <th>CSS grade</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <td>Other browsers</td>
+    <td>All others</td>
+    <td>-</td>
+    <td>-</td>
+    <td>U</td>
+    </tr>
+    </tbody>
+    <tfoot>
+    <tr>
+    <th>Rendering engine</th>
+    <th>Browser</th>
+    <th>Platform(s)</th>
+    <th>Engine version</th>
+    <th>CSS grade</th>
+    </tr>
+    </tfoot>
+    </table>
+    </div>
+    
+    </div>
+    
+    </div>
+    
+    </div>
+    
+    </div>
+    
+    </section>
+    
+    </div>
+    
+    <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+    <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    </footer>
+    
+    <aside class="control-sidebar control-sidebar-dark">
+    
+    </aside>
+    
+</div>
+    
+    </section>
+</body>
+@endsection
 
 <!-- ./wrapper -->
 
 @push('scripts')
-<!-- Select2 -->
-<script src={{asset("asset/plugins/select2/js/select2.full.min.js")}}></script>
-<!-- Bootstrap4 Duallistbox -->
-<script src={{asset("asset/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js")}}></script>
-<!-- InputMask -->
-<script src={{asset("asset/plugins/inputmask/jquery.inputmask.min.js")}}></script>
-<!-- bootstrap color picker -->
-<script src={{asset("asset/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js")}}></script>
-<!-- Bootstrap Switch -->
-<script src={{asset("asset/plugins/bootstrap-switch/js/bootstrap-switch.min.js")}}></script>
-<!-- BS-Stepper -->
-<script src={{asset("/plugins/bs-stepper/js/bs-stepper.min.js")}}></script>
-<!-- dropzonejs -->
-<script src={{asset("/plugins/dropzone/min/dropzone.min.js")}}></script>
 
+<script src={{asset("asset/plugins/jquery/jquery.min.js")}}></script>
+
+<script src={{asset("asset/plugins/bootstrap/js/bootstrap.bundle.min.js")}}></script>
+
+<script src={{asset("asset/plugins/datatables/jquery.dataTables.min.js")}}></script>
+<script src={{asset("asset/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js")}}></script>
+<script src={{asset("asset/plugins/datatables-responsive/js/dataTables.responsive.min.js")}}></script>
+<script src={{asset("asset/plugins/datatables-responsive/js/responsive.bootstrap4.min.js")}}></script>
+<script src={{asset("asset/plugins/datatables-buttons/js/dataTables.buttons.min.js")}}></script>
+<script src={{asset("asset/plugins/datatables-buttons/js/buttons.bootstrap4.min.js")}}></script>
+<script src={{asset("asset/plugins/jszip/jszip.min.js")}}></script>
+<script src={{asset("asset/plugins/pdfmake/pdfmake.min.js")}}></script>
+<script src={{asset("asset/plugins/pdfmake/vfs_fonts.js")}}></script>
+<script src={{asset("asset/plugins/datatables-buttons/js/buttons.html5.min.js")}}></script>
+<script src={{asset("asset/plugins/datatables-buttons/js/buttons.print.min.js")}}></script>
+<script src={{asset("asset/plugins/datatables-buttons/js/buttons.colVis.min.js")}}></script>
+
+<script src={{asset("asset/dist/js/adminlte.min.js?v=3.2.0")}}></script>
+
+<script src={{asset("asset/dist/js/demo.js")}}></script>
 
 <script>
   $(function () {
-    //Initialize Select2 Elements
-    $('.select2').select2()
-
-    //Initialize Select2 Elements
-    $('.select2bs4').select2({
-      theme: 'bootstrap4'
-    })
-
-    //Datemask dd/mm/yyyy
-    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-    //Datemask2 mm/dd/yyyy
-    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-    //Money Euro
-    $('[data-mask]').inputmask()
-
-    //Date picker
-    $('#reservationdate').datetimepicker({
-        format: 'L'
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
     });
-
-    //Date and time picker
-    $('#reservationdatetime').datetimepicker({ icons: { time: 'far fa-clock' } });
-
-    //Date range picker
-    $('#reservation').daterangepicker()
-    //Date range picker with time picker
-    $('#reservationtime').daterangepicker({
-      timePicker: true,
-      timePickerIncrement: 30,
-      locale: {
-        format: 'MM/DD/YYYY hh:mm A'
-      }
-    })
-    //Date range as a button
-    $('#daterange-btn').daterangepicker(
-      {
-        ranges   : {
-          'Today'       : [moment(), moment()],
-          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        startDate: moment().subtract(29, 'days'),
-        endDate  : moment()
-      },
-      function (start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-      }
-    )
-
-    //Timepicker
-    $('#timepicker').datetimepicker({
-      format: 'LT'
-    })
-
-    //Bootstrap Duallistbox
-    $('.duallistbox').bootstrapDualListbox()
-
-    //Colorpicker
-    $('.my-colorpicker1').colorpicker()
-    //color picker with addon
-    $('.my-colorpicker2').colorpicker()
-
-    $('.my-colorpicker2').on('colorpickerChange', function(event) {
-      $('.my-colorpicker2 .fa-square').css('color', event.color.toString());
-    })
-
-    $("input[data-bootstrap-switch]").each(function(){
-      $(this).bootstrapSwitch('state', $(this).prop('checked'));
-    })
-
-  })
-  // BS-Stepper Init
-  document.addEventListener('DOMContentLoaded', function () {
-    window.stepper = new Stepper(document.querySelector('.bs-stepper'))
-  })
-
-  // DropzoneJS Demo Code Start
-  Dropzone.autoDiscover = false
-
-  // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-  var previewNode = document.querySelector("#template")
-  previewNode.id = ""
-  var previewTemplate = previewNode.parentNode.innerHTML
-  previewNode.parentNode.removeChild(previewNode)
-
-  var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
-    url: "/target-url", // Set the url
-    thumbnailWidth: 80,
-    thumbnailHeight: 80,
-    parallelUploads: 20,
-    previewTemplate: previewTemplate,
-    autoQueue: false, // Make sure the files aren't queued until manually added
-    previewsContainer: "#previews", // Define the container to display the previews
-    clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
-  })
-
-  myDropzone.on("addedfile", function(file) {
-    // Hookup the start button
-    file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file) }
-  })
-
-  // Update the total progress bar
-  myDropzone.on("totaluploadprogress", function(progress) {
-    document.querySelector("#total-progress .progress-bar").style.width = progress + "%"
-  })
-
-  myDropzone.on("sending", function(file) {
-    // Show the total progress bar when upload starts
-    document.querySelector("#total-progress").style.opacity = "1"
-    // And disable the start button
-    file.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
-  })
-
-  // Hide the total progress bar when nothing's uploading anymore
-  myDropzone.on("queuecomplete", function(progress) {
-    document.querySelector("#total-progress").style.opacity = "0"
-  })
-
-  // Setup the buttons for all transfers
-  // The "add files" button doesn't need to be setup because the config
-  // `clickable` has already been specified.
-  document.querySelector("#actions .start").onclick = function() {
-    myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
-  }
-  document.querySelector("#actions .cancel").onclick = function() {
-    myDropzone.removeAllFiles(true)
-  }
-  // DropzoneJS Demo Code End
+  });
 </script>
+
 @endpush
 </body>
 </html>
 
-@endsection
+
